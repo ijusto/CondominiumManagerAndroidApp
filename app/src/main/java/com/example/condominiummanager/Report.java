9ee30fd03c821d64dbf412c5057151fca9eb26ae
Tomás Freitas <tomasfreitas@ua.pt>
2019-05-14 09:59:54 +0100

    Added some activities

---
+++ app/src/main/java/com/example/condominiummanager/Report.java
@@ -0,0 +1,22 @@
+package com.example.condominiummanager;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Report extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //TODO 'Thank you for submitting a report' -> go back to main screen
+    }
+
+}
