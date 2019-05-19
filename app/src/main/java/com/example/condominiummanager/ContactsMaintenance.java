6d4aab0a4488f6dfc99105c9fef7c9196ffdb2e2
Tomás Freitas <tomasfreitas@ua.pt>
2019-05-20 00:42:35 +0100

    alot of things

--- app/src/main/java/com/example/condominiummanager/ContactsMaintenance.java
+++ app/src/main/java/com/example/condominiummanager/ContactsMaintenance.java
@@ -1,6 +1,7 @@
 package com.example.condominiummanager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ContactsMaintenance extends AppCompatActivity {
 
@@ -17,6 +19,7 @@ public class ContactsMaintenance extends AppCompatActivity {
         setContentView(R.layout.activity_contacts_maintenance);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final SharedPreferences prefs = this.getSharedPreferences("com.example.condominiummanager", MODE_PRIVATE);
 
         final ImageView go_back = findViewById(R.id.go_back_contacts_maintenance);
 
@@ -44,6 +47,49 @@ public class ContactsMaintenance extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        final TextView tenantmobile = findViewById(R.id.contact_phone_tenant);
+        final TextView managermobile = findViewById(R.id.contact_phone_manager);
+
+        tenantmobile.setText(prefs.getString("tenantmobile", "something wrong"));
+        managermobile.setText(prefs.getString("managermobile", "something wrong"));
+
+        final TextView tenantemail = findViewById(R.id.contact_email_tenant);
+        final TextView manageremail = findViewById(R.id.contact_email_manager);
+
+        tenantemail.setText(prefs.getString("tenantemail", "something wrong"));
+        manageremail.setText(prefs.getString("manageremail", "something wrong"));
+        if (prefs.getString("tenantemailprivacy", "false").equals("false")) {
+            tenantemail.setVisibility(View.INVISIBLE);
+        }
+        if (prefs.getString("manageremailprivacy", "false").equals("false")) {
+            tenantemail.setVisibility(View.INVISIBLE);
+        }
+
+
+
     }
 
+    @Override
+    protected void onStart() {
+        final SharedPreferences prefs = this.getSharedPreferences("com.example.condominiummanager", MODE_PRIVATE);
+        super.onStart();
+        final TextView tenantmobile = findViewById(R.id.contact_phone_tenant);
+        final TextView managermobile = findViewById(R.id.contact_phone_manager);
+
+        tenantmobile.setText(prefs.getString("tenantmobile", "something wrong"));
+        managermobile.setText(prefs.getString("managermobile", "something wrong"));
+
+        final TextView tenantemail = findViewById(R.id.contact_email_tenant);
+        final TextView manageremail = findViewById(R.id.contact_email_manager);
+
+        tenantemail.setText(prefs.getString("tenantemail", "something wrong"));
+        manageremail.setText(prefs.getString("manageremail", "something wrong"));
+        if (prefs.getString("tenantemailprivacy", "false").equals("false")) {
+            tenantemail.setVisibility(View.INVISIBLE);
+        }
+        if (prefs.getString("manageremailprivacy", "false").equals("false")) {
+            tenantemail.setVisibility(View.INVISIBLE);
+        }
+    }
 }
