182756a31399947187179b67588b7af4ab3ee7af
Inês Justo <inesjusto@ua.pt>
2019-05-20 23:28:11 +0100

    add payments started

--- app/src/main/java/com/example/condominiummanager/PaymentsMaintenance.java
+++ app/src/main/java/com/example/condominiummanager/PaymentsMaintenance.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PaymentsMaintenance extends AppCompatActivity {
 
@@ -25,6 +26,50 @@ public class PaymentsMaintenance extends AppCompatActivity {
                 finish();
             }
         });
+
+        final ImageView add_payment = findViewById(R.id.add_payment);
+        final ImageView add_payment_rect_info = findViewById(R.id.add_payment_rect_info);
+        final TextView add_event_title = findViewById(R.id.add_event_title);
+        final TextView payment_name = findViewById(R.id.payment_name);
+        final TextView payment_name_input = findViewById(R.id.payment_name_input);
+        final TextView payment_amount = findViewById(R.id.payment_amount);
+        final TextView payment_amount_input = findViewById(R.id.payment_amount_input);
+        final ImageView check_payment = findViewById(R.id.check_payment);
+
+
+        add_payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add_payment_rect_info.setVisibility(View.VISIBLE);
+                add_event_title.setVisibility(View.VISIBLE);
+                payment_name.setVisibility(View.VISIBLE);
+                payment_name_input.setVisibility(View.VISIBLE);
+                payment_amount.setVisibility(View.VISIBLE);
+                payment_amount_input.setVisibility(View.VISIBLE);
+                add_payment.setVisibility((View.INVISIBLE));
+                check_payment.setVisibility((View.VISIBLE));
+            }
+        });
+
+        check_payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add_payment_rect_info.setVisibility(View.INVISIBLE);
+                add_event_title.setVisibility(View.INVISIBLE);
+                payment_name.setVisibility(View.INVISIBLE);
+                payment_name_input.setVisibility(View.INVISIBLE);
+                payment_amount.setVisibility(View.INVISIBLE);
+                payment_amount_input.setVisibility(View.INVISIBLE);
+                add_payment.setVisibility((View.VISIBLE));
+                check_payment.setVisibility((View.INVISIBLE));
+            }
+        });
+
+
+
+
+
+
     }
 
 }
